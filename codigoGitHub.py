f2d0cbbe8e3a355a5961311a55f6f06e7e821d38
gabrielalopez1 <163205409+gabrielalopez1@users.noreply.github.com>
2024-04-01 18:19:55 -0500

    cree el diccionario
    además del diccionario, la lista y el contador para cuando se cumplan los 3 articulos

---
+++ codigoGitHub.py
@@ -0,0 +1,24 @@
+
+compras = []
+total_compra = 0
+cont = 0
+zapatos = {
+    1: ["sandalias","negras", 10], 
+    2: ["botas","negras", 15], 
+    3: ["tenis","blancos", 12], 
+    4: ["tenis","rojos", 12]
+}
+jeans = {
+    1: ["clásico", "azul", 20], 
+    2: ["clásico", "negro", 20], 
+    3: ["con desgastes", "azul", 15], 
+    4: ["con desgastes", "gris", 15]
+}
+camisetas = {
+    1: ["polo", "blanca", "20"], 
+    2: ["polo", "azul", "20"], 
+    3: ["cuello V", "negra", "15"], 
+    4: ["cuello V", "lila", "15"]
+}
+
+
